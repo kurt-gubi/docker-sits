79d37b1228a5deb6f93fa91fc200dfdd5ed211d2
Kurt Gubi <kurt.gubi@sydney.edu.au>
2019-11-05 17:09:29 +1100

    Initial commit of minimally functioning Dockerfile & resources for Oracle 11.2.0.3 install, taken directly from old Packer scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # docker-sits
-Containerising SITS
+
+A conversion, from Packer to Puppet to Docker, of my work automating the provisioning of [SITS](https://www.tribalgroup.com/software-and-services/student-information-systems/sitsvision/ environments at the University of Sydney.
---
+++ oracle-11.2.0.3/provision/setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+umask 002
+
+# install Oracle dependencies
+yum -y install binutils compat-libcap1 compat-libcap1.i686 compat-libstdc++-33 compat-libstdc++-33.i686 elfutils-libelf elfutils-libelf-devel gcc gcc-c++ glibc glibc.i686 glibc-common glibc-devel glibc-devel.i686 glibc-headers ksh libaio libaio.i686 libaio-devel libaio-devel.i686 libgcc libgcc.i686 libstdc++ libstdc++.i686 libstdc++-devel libstdc++-devel.i686 make numactl-devel sysstat unixODBC unixODBC.i686 unixODBC-devel
+# install other dependencies
+yum -y install zip unzip libXtst
+
+find /tmp/provision -name '*.sh' -exec chmod a+x {} \;
+
+cd /tmp/
+
+# setup users / groups
+groupadd -g 501 oinstall
+groupadd -g 502 dba
+groupadd -g 503 oper
+useradd -m -u 501 -g oinstall -G dba,oper -d /home/oracle -s /bin/bash -c "Oracle Software Owner" oracle
+mkdir -p /u01/app/oracle
+mkdir /u01/app/oraInventory
+chown -R oracle:dba /u01/app
+
+# deploy bash_profile
+cp /tmp/provision/oracle.bash_profile /home/oracle/.bash_profile
+chown oracle:oinstall /home/oracle/.bash_profile
+
+# extract installation files
+unzip /tmp/p10404530_112030_Linux-x86-64_1of7.zip -d /tmp/
+unzip /tmp/p10404530_112030_Linux-x86-64_2of7.zip -d /tmp/
+chown -R oracle:oinstall /tmp/database
+
+# run pre-install scripts
+cp -r /tmp/provision/oracle-install /tmp/
+chmod u+x /tmp/oracle-install/*.sh
+/tmp/oracle-install/runfixup.sh
+
+# changing "$(MK_EMAGENT_NMECTL)" to "$(MK_EMAGENT_NMECTL) -lnnz11" in ins_emagent.mk
+# ( https://community.oracle.com/docs/DOC-921694 )
+unzip /tmp/database/stage/Components/oracle.sysman.agent/10.2.0.4.3/1/DataFiles/filegroup38.jar -d /tmp/filegroup38
+sed -i 's/^\(\s*\$(MK_EMAGENT_NMECTL)\)\s*$/\1 -lnnz11/g' /tmp/filegroup38/sysman/lib/ins_emagent.mk
+cd /tmp/filegroup38
+zip -r filegroup38.jar sysman
+cd /tmp
+mv /tmp/database/stage/Components/oracle.sysman.agent/10.2.0.4.3/1/DataFiles/filegroup38.jar /tmp/database/stage/Components/oracle.sysman.agent/10.2.0.4.3/1/DataFiles/filegroup38.bak
+mv /tmp/filegroup38/filegroup38.jar /tmp/database/stage/Components/oracle.sysman.agent/10.2.0.4.3/1/DataFiles/
+chown oracle:oinstall /tmp/database/stage/Components/oracle.sysman.agent/10.2.0.4.3/1/DataFiles/filegroup38.jar
+
+# install oracle
+su - oracle -c "/tmp/database/runInstaller -silent -ignorePrereq -noconfig -waitforcompletion -responseFile /tmp/oracle-install/response.11-2-0-3.rsp"
+/u01/app/oraInventory/orainstRoot.sh
+/u01/app/oracle/product/11.2.0/dbhome_1/root.sh
+
+# create "SITS" database
+su - oracle -c "dbca -silent -createDatabase -templateName General_Purpose.dbc -sid sits -sysPassword sits -systemPassword sits -gdbName sits -emConfiguration NONE -storageType FS -datafileDestination /u01/app/oracle/oradata -datafileJarLocation /u01/app/oracle/product/11.2.0/dbhome_1/assistants/dbca/templates -responseFile NO_VALUE -characterset WE8MSWIN1252 -obfuscatedPasswords false -sampleSchema true -oratabLocation /etc/oratab -automaticMemoryManagement true -totalMemory 800 -maskPasswords false -oui_internal"
+sed -i "s/\(sits:.*\):N$/\1:Y/" /etc/oratab
+echo -e "\nDEFAULT_SERVICE_LISTENER=sits" >> /u01/app/oracle/product/11.2.0/dbhome_1/network/admin/listener.ora
+
+su - oracle -c "echo 'ALTER SYSTEM SET open_cursors = 2000 SCOPE=BOTH;' | sqlplus / as sysdba"
+su - oracle -c "echo 'ALTER SYSTEM SET processes = 300 SCOPE=spfile;' | sqlplus / as sysdba"
+su - oracle -c "echo 'ALTER SYSTEM SET sessions = 300 SCOPE=spfile;' | sqlplus / as sysdba"
+su - oracle -c "echo 'ALTER SYSTEM SET db_files = 400 SCOPE=spfile;' | sqlplus / as sysdba"
+su - oracle -c "echo 'ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;' | sqlplus / as sysdba"
+
+# deploy startup script
+cp /tmp/provision/init-d.dbora /etc/init.d/dbora
+chmod 750 /etc/init.d/dbora
+chkconfig --level 345 dbora on
+
+# tidy up files
+rm -rf /tmp/CVU_11.2.0.3.0_oracle
+rm -rf /tmp/database
+rm -rf /tmp/dbca
+rm -rf /tmp/filegroup38
+rm -rf /tmp/logs
+rm -rf /tmp/oracle-install
+rm -f /tmp/p10404530_112030_Linux-x86-64_1of7.zip
+rm -f /tmp/p10404530_112030_Linux-x86-64_2of7.zip
